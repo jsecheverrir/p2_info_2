15d0e07a939a5fef301c278a3942dd1ce8e4cc98
Sebastian-8a <s.ochoa@udea.edu.co>
2023-10-31 21:12:13 -0500

    Eliminar código residual

--- Othello/main.cpp
+++ Othello/main.cpp
@@ -22,35 +22,5 @@ int main(){
     }
     prueba.contar_fichas( & cant_fichas_negro, &cant_fichas_blanco);
 
-    /*
-
-
-prueba.imprimir_tablero();
-    prueba.push_back(0);
-    prueba.push_back(1);
-    prueba.push_back(1);
-    prueba.push_back(2);
-    prueba.push_back(2);
-    prueba.push_back(3);
-    prueba.push_back(3);
-    prueba.push_back(4);
-    prueba.push_back(4);
-    prueba.push_back(5);
-    prueba.push_back(5);
-    prueba.push_back(6);
-    prueba.mostrar_movimientos_posibles();
-
-
-
-
-    prueba.mostrar_movimientos_posibles();
-    cout << "\n";
-    cout << prueba.get_capacidad();
-    cout << "\n";
-
-
-*/
-
-
     return 0;
 }
