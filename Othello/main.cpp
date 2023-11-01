f0989531ef8a5cdd553cc2b199a42795e5358b96
Sebastian-8a <s.ochoa@udea.edu.co>
2023-11-01 08:04:36 -0500

    Cambio nombre clase y pequeños detalles

--- Othello/main.cpp
+++ Othello/main.cpp
@@ -1,28 +1,28 @@
 #include "clases.h"
 
 int main(){
-    tablero prueba;
+    tablero juego;
     char jugadores[2] = {'-','*'},jugador = ' ';
     int sin_movimientos = 0;        //cantidad de veces que se pueden quedar sin movimientos
     int contador = 0,cant_fichas_negro = 0,cant_fichas_blanco = 0;
     while (sin_movimientos < 2){
         jugador = (contador % 2 == 0)? jugadores[0] : jugadores[1] ;
-        prueba.calcular_mov_legales(jugador);
-        if (prueba.get_tamaño()==0){
+        juego.calcular_mov_legales(jugador);
+        if (juego.get_tamaño()==0){
             sin_movimientos++;
             contador ++;
         }
         else{
             sin_movimientos = 0;
-            prueba.mostrar_movimientos_posibles();
-            prueba.movimiento(jugador);
-            prueba.vaciar_movimientos();
+            juego.mostrar_movimientos_posibles();
+            juego.movimiento(jugador);
+            juego.vaciar_movimientos();
             contador++;
         }
     }
     int ganador = '='; // Inicialmente no hay ganador
     // Contar las fichas en el tablero
-    prueba.contar_fichas(&cant_fichas_negro, &cant_fichas_blanco);
+    juego.contar_fichas(&cant_fichas_negro, &cant_fichas_blanco);
     if (cant_fichas_negro > cant_fichas_blanco) {
         cout << "El ganador es el Jugador 1 con " << cant_fichas_negro << " fichas." << endl;
         ganador = '-';
@@ -35,9 +35,9 @@ int main(){
     }
     // Solicitar nombres de los jugadores
     string nombreJugador1, nombreJugador2;
-    cout << "Ingrese el nombre del Jugador 1: ";
+    cout << "Ingrese el nombre del Jugador -: ";
     cin >> nombreJugador1;
-    cout << "Ingrese el nombre del Jugador 2: ";
+    cout << "Ingrese el nombre del Jugador *: ";
     cin >> nombreJugador2;
     // Guardar la partida
     guardarPartida(nombreJugador1, nombreJugador2, ganador);
